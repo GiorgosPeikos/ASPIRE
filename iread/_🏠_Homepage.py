0b00e18e7f7476230bb48ff10d344b43effbea4c
wojciechkusa <wojciech.kusa@allegro.com>
2024-05-23 17:27:37 +0200

    add basic description to homepage

--- iread/_🏠_Homepage.py
+++ iread/_🏠_Homepage.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from utils.utils import load_css
 
+
 def main():
     st.set_page_config(layout="wide")
     load_css('iread/css/styles.css')
-
 
     with st.sidebar:
         if st.button("Main Page"):
             st.session_state['page'] = 'Run Selection and Information'
         if st.button("File Submissions"):
             st.session_state['page'] = 'Submit Experiments for Analysis'
+
+    st.title("Interactive Dashboard for IR Evaluation")
+    st.write("This application allows you to analyze retrieval experiments, "
+             "compare multiple retrieval_runs, and obtain various visualizations.")
 
 
 if __name__ == "__main__":
--- iread/utils/utils.py
+++ iread/utils/utils.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import os
 
 
-def load_css(file_name):
+def read_data(folder_path: str):
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    data_frames = {file: pd.read_csv(os.path.join(folder_path, file)) for file in files}
+    return data_frames
+
+
+def load_css(file_name: str):
     with open(file_name, "r") as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)

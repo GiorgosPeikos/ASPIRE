adc1c0591bff9673d5ca06fcfc58529a780b3771
Gpeik <georg95@hotmail.gr>
2024-07-09 01:01:06 +0200

    - TODO: Test that the PyTerrier and this code give the same stat. sign. results for the same runs.
    - Improve the dataframe creation function: create_results_dataframe so that the results are presented nicely.

--- iread/pages/Multiple_Experiment_Report.py
+++ iread/pages/Multiple_Experiment_Report.py
@@ -70,104 +70,104 @@ if st.button("Begin the Experimental Evaluation!", key='me_stButtonCenter'):
 st.divider()
 
 
-# Overall Retrieval Characteristics
-# with st.container():
-#     st.markdown("""<h3>Retrieval Performance - <span style="color:red;">Overall Retrieval Characteristics</span></h3>""", unsafe_allow_html=True)
-#
-#     if 'me_selected_qrels' not in st.session_state:
-#         st.warning("Please select retrieval experiment and qrels to begin your evaluation.", icon="⚠")
-#     else:
-#         st.session_state.me_relevance_threshold = st.slider(
-#             "Select from the Available Relevance Thresholds (Slide)",
-#             min_value=1,
-#             max_value=2,
-#             value=1,
-#             key="me_slider1"
-#         )
-#
-#         if 'me_prev_relevance_threshold' not in st.session_state:
-#             st.session_state.me_prev_relevance_threshold = 1
-#
-#         if st.session_state.me_relevance_threshold != st.session_state.me_prev_relevance_threshold:
-#             st.session_state.me_prev_relevance_threshold = st.session_state.me_relevance_threshold
-#
-#         # Get the list of selected run files
-#         me_selected_runs_files = list(st.session_state.me_selected_runs.keys())
-#
-#         if me_selected_runs_files:
-#             columns = st.columns(2)
-#             _, _, _, _, overall_measures, precision_measures = return_available_measures()
-#
-#             overall_measures_combined = pd.DataFrame()
-#             precision_measures_combined = pd.DataFrame()
-#             recall_measures_combined = pd.DataFrame()  # New DataFrame for recall measures
-#
-#             progress_bar = st.empty()
-#             progress_text = st.empty()
-#             total_files = len(selected_runs_files)
-#
-#             for i, run_file in enumerate(selected_runs_files):
-#                 progress = (i + 1) / total_files
-#                 progress_bar.progress(progress)
-#                 progress_text.text(f"Evaluating experiment {i + 1}/{total_files}")
-#
-#                 with columns[0]:
-#                     overall_measures_results = {}
-#                     for measure_name in overall_measures:
-#                         overall_measures_results[measure_name] = evaluate_single_run(
-#                             st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], measure_name, st.session_state.me_relevance_threshold
-#                         )
-#
-#                     df_measures = pd.DataFrame([overall_measures_results],
-#                                                index=[run_file.replace('.txt', '')])
-#
-#                     df_measures = df_measures.rename(columns={
-#                         "NumQ": "Total Queries",
-#                         "NumRet": "Retrieved Documents",
-#                         "NumRel": "Relevant Documents",
-#                         "NumRelRet": "Relevant Retrieved Documents"
-#                     })
-#
-#                     # Add overall measures to the combined DataFrame
-#                     overall_measures_combined = pd.concat([overall_measures_combined, df_measures.transpose()], axis=1)
-#
-#                 with columns[1]:
-#                     precision_measures_results = {}
-#                     for measure_name in precision_measures:
-#                         precision_measures_results[measure_name] = evaluate_single_run(
-#                             st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], measure_name, st.session_state.me_relevance_threshold
-#                         )
-#
-#                     df_prec_measures = pd.DataFrame(precision_measures_results, index=[run_file.replace('.txt', '')]).transpose()
-#
-#                     # Add precision measures to the combined DataFrame
-#                     precision_measures_combined[run_file.replace('.txt', '')] = df_prec_measures.iloc[:, 0]
-#
-#                     # Calculate and add recall measures to the combined DataFrame
-#                     recall_measures_results = {
-#                         "Recall@50": evaluate_single_run(
-#                             st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], "R@50", st.session_state.me_relevance_threshold
-#                         ),
-#                         "Recall@1000": evaluate_single_run(
-#                             st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], "R@1000", st.session_state.me_relevance_threshold
-#                         )
-#                     }
-#                     df_recall_measures = pd.DataFrame([recall_measures_results], index=[run_file.replace('.txt', '')])
-#                     recall_measures_combined = pd.concat([recall_measures_combined, df_recall_measures.transpose()], axis=1)
-#
-#             # Clear the progress bar and text
-#             progress_bar.empty()
-#             progress_text.empty()
-#
-#             # Display the combined DataFrames
-#             st.markdown("<h4>Overall Measures Combined</h4>", unsafe_allow_html=True)
-#             st.dataframe(overall_measures_combined, use_container_width=True)
-#
-#             st.markdown("<h4>Precision Measures Combined</h4>", unsafe_allow_html=True)
-#             st.dataframe(precision_measures_combined, use_container_width=True)
-#
-#             st.markdown("<h4>Recall Measures Combined</h4>", unsafe_allow_html=True)
-#             st.dataframe(recall_measures_combined, use_container_width=True)
+Overall Retrieval Characteristics
+with st.container():
+    st.markdown("""<h3>Retrieval Performance - <span style="color:red;">Overall Retrieval Characteristics</span></h3>""", unsafe_allow_html=True)
+
+    if 'me_selected_qrels' not in st.session_state:
+        st.warning("Please select retrieval experiment and qrels to begin your evaluation.", icon="⚠")
+    else:
+        st.session_state.me_relevance_threshold = st.slider(
+            "Select from the Available Relevance Thresholds (Slide)",
+            min_value=1,
+            max_value=2,
+            value=1,
+            key="me_slider1"
+        )
+
+        if 'me_prev_relevance_threshold' not in st.session_state:
+            st.session_state.me_prev_relevance_threshold = 1
+
+        if st.session_state.me_relevance_threshold != st.session_state.me_prev_relevance_threshold:
+            st.session_state.me_prev_relevance_threshold = st.session_state.me_relevance_threshold
+
+        # Get the list of selected run files
+        me_selected_runs_files = list(st.session_state.me_selected_runs.keys())
+
+        if me_selected_runs_files:
+            columns = st.columns(2)
+            _, _, _, _, overall_measures, precision_measures = return_available_measures()
+
+            overall_measures_combined = pd.DataFrame()
+            precision_measures_combined = pd.DataFrame()
+            recall_measures_combined = pd.DataFrame()  # New DataFrame for recall measures
+
+            progress_bar = st.empty()
+            progress_text = st.empty()
+            total_files = len(selected_runs_files)
+
+            for i, run_file in enumerate(selected_runs_files):
+                progress = (i + 1) / total_files
+                progress_bar.progress(progress)
+                progress_text.text(f"Evaluating experiment {i + 1}/{total_files}")
+
+                with columns[0]:
+                    overall_measures_results = {}
+                    for measure_name in overall_measures:
+                        overall_measures_results[measure_name] = evaluate_single_run(
+                            st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], measure_name, st.session_state.me_relevance_threshold
+                        )
+
+                    df_measures = pd.DataFrame([overall_measures_results],
+                                               index=[run_file.replace('.txt', '')])
+
+                    df_measures = df_measures.rename(columns={
+                        "NumQ": "Total Queries",
+                        "NumRet": "Retrieved Documents",
+                        "NumRel": "Relevant Documents",
+                        "NumRelRet": "Relevant Retrieved Documents"
+                    })
+
+                    # Add overall measures to the combined DataFrame
+                    overall_measures_combined = pd.concat([overall_measures_combined, df_measures.transpose()], axis=1)
+
+                with columns[1]:
+                    precision_measures_results = {}
+                    for measure_name in precision_measures:
+                        precision_measures_results[measure_name] = evaluate_single_run(
+                            st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], measure_name, st.session_state.me_relevance_threshold
+                        )
+
+                    df_prec_measures = pd.DataFrame(precision_measures_results, index=[run_file.replace('.txt', '')]).transpose()
+
+                    # Add precision measures to the combined DataFrame
+                    precision_measures_combined[run_file.replace('.txt', '')] = df_prec_measures.iloc[:, 0]
+
+                    # Calculate and add recall measures to the combined DataFrame
+                    recall_measures_results = {
+                        "Recall@50": evaluate_single_run(
+                            st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], "R@50", st.session_state.me_relevance_threshold
+                        ),
+                        "Recall@1000": evaluate_single_run(
+                            st.session_state.me_selected_qrels, st.session_state.me_selected_runs[run_file], "R@1000", st.session_state.me_relevance_threshold
+                        )
+                    }
+                    df_recall_measures = pd.DataFrame([recall_measures_results], index=[run_file.replace('.txt', '')])
+                    recall_measures_combined = pd.concat([recall_measures_combined, df_recall_measures.transpose()], axis=1)
+
+            # Clear the progress bar and text
+            progress_bar.empty()
+            progress_text.empty()
+
+            # Display the combined DataFrames
+            st.markdown("<h4>Overall Measures Combined</h4>", unsafe_allow_html=True)
+            st.dataframe(overall_measures_combined, use_container_width=True)
+
+            st.markdown("<h4>Precision Measures Combined</h4>", unsafe_allow_html=True)
+            st.dataframe(precision_measures_combined, use_container_width=True)
+
+            st.markdown("<h4>Recall Measures Combined</h4>", unsafe_allow_html=True)
+            st.dataframe(recall_measures_combined, use_container_width=True)
 
 st.divider()
 

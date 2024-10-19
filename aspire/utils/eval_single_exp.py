efe1266707a86affa8e30a2e4cf2511a9946a9a0
Gpeik <georg95@hotmail.gr>
2024-10-19 10:10:06 +0200

    Page 1 update: Cell font change, page 1

--- aspire/utils/eval_single_exp.py
+++ aspire/utils/eval_single_exp.py
@@ -185,7 +185,7 @@ def create_results_table(statistical_results):
 
                 if mean == max_mean:
                     style.append("font-weight: bold")
-                    # Removed text-decoration: underline !important as we're now underlining in the string itself
+                    # Removed text-decoration
 
                 style_df.loc[system, metric] = "; ".join(style)
 
